4385f5ffe4f710e48011ce53d097e9728d666426
Jared Thomas <jjthom87@yahoo.com>
2023-07-16 21:50:37 -0400

    adding comments to the javascript

--- array-script.js
+++ array-script.js
@@ -1,27 +1,36 @@
+//creating an array of strings and storing it in a variable appropriately called 'users'
 let users = ["bob", "jim"];
+//creating an array of strings and storing it in a variable appropriately called 'greetings'
 let greetings = ['world', 'goodbye', "hello"];
-
+//logging the value of the 'greetings' array
 console.log(greetings);
-
+//adding a string called "bonjour" to the end of the 'greetings' array
 greetings.push("bonjour");
-
+//logging the value of the 'greetings' array to show that it has been manipulated after .push()
 console.log(greetings);
-
+//removing the last index in the greetings array and storing it in a variable called 'lastIndexInGreetingsArray'
 const lastIndexInGreetingsArray = greetings.pop();
+//logging the value of the 'lastIndexInGreetingsArray' variable
 console.log(lastIndexInGreetingsArray)
-
+//logging the value of the 'greetings' array to show that is been manipulated after .pop() was called
 console.log(greetings);
-
+//adding a string called "bonjour" to the end of the 'greetings' array
 greetings.push("bonjour");
+//adding a string called "welcome" to the end of the 'greetings' array
 greetings.push("welcome");
-
+//logging the value of the 'greetings' array to show that it has been manipulated after .push()
 console.log(greetings);
+//starting at the 1 index, removing 2 items. so, index 1 & 2 are removed
 greetings.splice(1, 2);
+//logging the value of the 'greetings' array to show that it has been manipulated after .splice()
 console.log(greetings);
-
-// console.log(greetings.length);
-
+//logging the length of the greeting array after manipulation
+console.log(greetings.length);
+//creating a new array and storing it in a variable named 'namesArray'
 let namesArray = new Array();
+//adding a string called "Joe" to the end of the 'namesArray' array
 namesArray.push("Joe")
+//adding a string called "Bob" to the end of the 'namesArray' array
 namesArray.push("Bob")
+//logging the value of the 'namesArray' variable
 console.log(namesArray)
--- object-script.js
+++ object-script.js
@@ -1,3 +1,7 @@
+//creating an object and storing it in a variable named "janeDoePerson"
+//giving the object properties of name, age, phone, hasDiploma, felonies
+//giving values to all of those properties
+//object value for name, integer value of age, string value for phone, boolean value for hasDiploma, array value for felonies
 const janeDoePerson = {
     name: {first: "Jane", last: "Doe"},
     age: 25,
@@ -5,7 +9,29 @@ const janeDoePerson = {
     hasDiploma: true,
     felonies: ["arson", "murder"]
 }
+//logging the value of the janeDoePerson object variable
+console.log(janeDoePerson)
+//logging the hasDiploma property value for the janeDoePerson
+console.log(janeDoePerson.hasDiploma)
+//logging the hasDiploma property value for the janeDoePerson
+console.log(janeDoePerson['hasDiploma'])
+//adding an address property to the janeDoePerson object with a value of 'CitiField'
+janeDoePerson.address = "CitiField"
+//logging the value of the updated janeDoePerson object
+console.log(janeDoePerson)
+//adding a 'hairColor' property to the janeDoePerson object with a value of 'brown'
+janeDoePerson['hairColor'] = "brown"
+//logging the value of the updated janeDoePerson object
+console.log(janeDoePerson)
+//adding a 'favoriteFruits' property to the janeDoePerson object with a value of 'brown'
+janeDoePerson.favoriteFruits = ["apples", "oranges", "kiwi"]
+//logging the value of the updated janeDoePerson object
+console.log(janeDoePerson)
 
+//creating another very similar object and storing it in a variable named "johnDoePerson"
+//giving the object properties of name, age, phone, hasDiploma, felonies
+//giving values to all of those properties
+//object value for name, integer value of age, string value for phone, boolean value for hasDiploma, array value for felonies
 const johnDoePerson = {
     name: {first: "John", last: "Doe"},
     age: 35,
@@ -13,22 +39,18 @@ const johnDoePerson = {
     hasDiploma: false,
     felonies: ["drug possession", "drug sales"]
 }
+//adding (push) a felony to the felonies array for the johnDoePerson object
+johnDoePerson.felonies.push("grand theft auto")
+//logging the value of the updated johnDoePerson object
+console.log(johnDoePerson)
 
+//creating an empty array and storing it in a variable called mostWantedListPeople 
 const mostWantedListPeople = []
+//adding the janeDoePerson object variable to the end of the mostWantedListPeople array
 mostWantedListPeople.push(janeDoePerson);
+//adding the johnDoePerson object variable to the end of the mostWantedListPeople array
 mostWantedListPeople.push(johnDoePerson);
+//since they are both most wanted people, it makes sense that they belong in the same array
 
+//logging the value of the mostWantedListPeople array
 console.log(mostWantedListPeople)
-
-// console.log(janeDoePerson)
-
-// console.log(janeDoePerson.hasDiploma)
-// console.log(janeDoePerson['hasDiploma'])
-
-// janeDoePerson.address = "CitiField"
-// console.log(janeDoePerson)
-// janeDoePerson['hairColor'] = "brown"
-// console.log(janeDoePerson)
-
-// janeDoePerson.favoriteFruits = ["apples", "oranges", "kiwi"]
-// console.log(janeDoePerson)
